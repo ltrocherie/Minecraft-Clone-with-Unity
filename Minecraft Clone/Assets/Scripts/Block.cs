2ace4b3963c34414615b3fbb047be23edc6e0620
lucastrocherie <lucas.trocherie72@gmail.com>
2020-06-08 16:17:25 +0200

    Repaired the project

---
+++ Minecraft Clone/Assets/Scripts/Block.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Block 
+{
+    int x, y, z;
+
+    public Block(int x, int y, int z)
+    {
+
+    }
+
+}
+
+public enum BlockType { Air, Ground };
